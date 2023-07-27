b47f467d09ff348c9074149829b28be02e41fb19
Weslley <weslley.washington01486@gmail.com>
2023-07-26 23:01:48 -0300

    starting the first stage of the website

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,60 @@
-import {CampoPrincipal, Container } from "./style.styles"
+import {
+    SectionApresentacao,
+    CampoImg, 
+    CampoNetwork, 
+    CampoPrincipal, 
+    SectionRedes, 
+    CampoTexto, 
+    CampoTitulo, 
+    Container, 
+    ImagemL,
+    ImagemG,
+    ImagemPerfil, 
+    Img, 
+    LinkParaLinkedin, 
+    Paragrafo, 
+    Titulo, 
+    LinkParaGithub} from "./style.styles"
 
+import imageLinkedin from '../../assets/img/img-linkedin.png'
+import imagemPerfil from '../../assets/img/img-perfil.png'
+import imageCity from '../../assets/img/apresentacao.png'
+import imageGithub from '../../assets/img/github.png'
 
 export const Main = () => {
     return (
         <>
         <Container>
             <CampoPrincipal>
-            
+                <SectionApresentacao>
+                    <Img src={imageCity}></Img>
+                    <CampoImg>
+                    <ImagemPerfil src={imagemPerfil}></ImagemPerfil>
+                    </CampoImg>
+                    <CampoTitulo>
+                        <Titulo>Olá Meu Nome é Weslley Washington.</Titulo>
+                    </CampoTitulo>
+                    <CampoTexto>
+                        <Paragrafo>Eu sou Desenvolvedor Front-end Jr. Trabalho com HTML, CSS, JavaScript e React</Paragrafo>
+                    </CampoTexto>
+                </SectionApresentacao>
+
+                <SectionRedes>
+                    <CampoNetwork>
+                        <LinkParaLinkedin href="https://www.linkedin.com/in/weslley-washington/" 
+                            target='_blank'>
+                            <ImagemL src={imageLinkedin}></ImagemL>
+                        </LinkParaLinkedin>
+
+                        <LinkParaGithub href="https://github.com/WeslleyWashington" target="_blank" >
+                            <ImagemG src={imageGithub}></ImagemG>
+                        </LinkParaGithub>
+                        
+                    </CampoNetwork>
+                </SectionRedes>
+                
+
+                
             </CampoPrincipal> 
         </Container>
         </>
--- src/components/Main/style.styles.ts
+++ src/components/Main/style.styles.ts
@@ -1,12 +1,97 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
-    display: flex;
     width: 100%;
-    height: 100%;  
+    height: 100%; 
 `
-export const CampoPrincipal = styled.div`
-background: linear-gradient(24deg, #0e1e66 15%, #7905f3 100%);
-width: 100vw;
-height: 100vh;
+export const CampoPrincipal = styled.body`
+    background: linear-gradient(24deg, #0e1e66 15%, #7905f3 100%);
+    width: 100%;
+    height: 100%;
+`
+
+export const SectionApresentacao = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 15rem;
+`
+export const Img = styled.img`
+    width: 100%;
+    position: absolute;
+    box-shadow: rgb(0 0 0) 0px 7px 14px 2px;
+`
+
+
+
+export const CampoTitulo = styled.div`
+    z-index: 2;
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    position: relative;
+    top: 9.68rem;
+`
+
+export const Titulo = styled.h1``
+
+export const CampoImg = styled.div`
+    position: relative;
+    top: 9.68rem;
+
+`
+export const ImagemPerfil = styled.img`
+    width: 15rem;
+    height: auto;
+    border-radius: 8px;
+    box-shadow: rgb(0 0 0) 0px 7px 14px 2px;
+`
+
+export const CampoTexto = styled.div`
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    position: relative;
+    top: 9.68rem;
+`
+
+export const Paragrafo = styled.p``
+
+export const SectionRedes = styled.section``
+
+export const CampoNetwork = styled.div`
+    padding: 2rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+`
+export const LinkParaLinkedin = styled.a`
+
+`
+export const LinkParaGithub = styled.a``
+
+export const ImagemL = styled.img`
+    width: 35rem;
+    height: auto;
+    border-radius: 9px;
+    transition: 0.2s ease;
+
+    &:hover {
+        box-shadow: rgba(0, 0, 0, 0.5) 0px 7px 14px 2px;
+        width: 35.03rem;
+    }
+    
+
+`
+export const ImagemG = styled.img`
+    width: 42rem;
+    height: auto;
+    border-radius: 9px;
+    transition: 0.2s ease;
+
+    &:hover {
+        box-shadow: rgba(0, 0, 0, 0.5) 0px 7px 14px 2px;
+        width: 42.03rem;
+    }
+    
+
 `
